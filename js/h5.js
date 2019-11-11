b4a9d6adf3256dab0b47dab622f9de7955f6d6d9
unknown <hahahahchenshan@163.com>
2019-11-11 12:04:06 +0800

    20191111csc update

--- js/h5.js
+++ js/h5.js
@@ -2,48 +2,108 @@
  * @Author: ChenShan 
  * @Date: 2019-11-07 23:58:11 
  * @Last Modified by: ChenShan
- * @Last Modified time: 2019-11-08 13:35:48
+ * @Last Modified time: 2019-11-10 18:25:38
  */
 
-var width = 600;
-var height = 600;
-var svg = d3.select("body")
-    .append("svg")
-    .attr("width",width)
-    .attr("height",height); 
+const width = 500
+const height = 400
 
-d3.json("../data/h5.json", function(error, root) {
-    var nodes = treemap.nodes(root);
-    var links = treemap.links(nodes);
-    
-    console.log(nodes);
-    console.log(links);
+const treeData = {
+    'name': '中国',
+    'children': [
+    {
+        'name': '浙江',
+        'children':
+        [
+        {'name': '杭州', 'gdp': 1234},
+        {'name': '宁波', 'gdp': 3334},
+        {'name': '温州', 'gdp': 2000},
+        {'name': '绍兴', 'gdp': 1002}
+        ]
+    },
+    {
+        'name': '广西',
+        'children': [
+        {
+            'name': '桂林',
+            'children':
+            [
+            {'name': '秀峰区', 'gdp': 2131},
+            {'name': '叠彩区', 'gdp': 2015},
+            {'name': '象山区', 'gdp': 988},
+            {'name': '七星区', 'gdp': 756}
+            ]
+        },
+        {'name': '南宁', 'gdp': 3699},
+        {'name': '柳州', 'gdp': 4511},
+        {'name': '防城港', 'gdp': 2325}
+        ]
+    },
+    {
+        'name': '黑龙江',
+        'children': [
+        {'name': '哈尔滨', 'gdp': 784},
+        {'name': '齐齐哈尔', 'gdp': 885},
+        {'name': '牡丹江', 'gdp': 1254},
+        {'name': '大庆', 'gdp': 3240}
+        ]
+    },
+    {
+        'name': '新疆',
+        'children':
+        [
+            {'name': '乌鲁木齐', 'gdp': 2456},
+            {'name': '克拉玛依', 'gdp': 1015},
+            {'name': '吐鲁番', 'gdp': 998},
+            {'name': '哈密', 'gdp': 654}
+        ]
+    }
+    ]
+}
 
-    var treemap = d3.layout.treemap()
-    .size([width, height])
-    .value(function(d){ return d.gdp; });
-    
-    var groups = svg.selectAll("g")
-        .data(nodes.filter(function(d){ return !d.children; }))
-        .enter()
-        .append("g");
-                
-    var rects = groups.append("rect")
-            .attr("class","nodeRect")
-            .attr("x",function(d){ return d.x; })
-            .attr("y",function(d){ return d.y; })
-            .attr("width",function(d){ return d.dx; })
-            .attr("height",function(d){ return d.dy; })
-            .style("fill",function(d,i){ 
-    return "color(d.parent.name)"; });
+const color = d3.scaleOrdinal(d3.schemeCategory10)
 
-    var texts = groups.append("text")
-            .attr("class","nodeName")
-            .attr("x",function(d){ return d.x; })
-            .attr("y",function(d){ return d.y; })
-            .attr("dx","0.5em")
-            .attr("dy","1.5em")
-            .text(function(d){ 
-                return d.name + " " + d.gdp; 
-            });
-})
+// 数据转化
+const treemap = d3.treemap().size([width, height])
+const root = d3.hierarchy(treeData).sum((d) => d.gdp)
+const tree = treemap(root) // 获取treemap结构树
+const leaves = tree.leaves() // 将生成的树形结构转化成叶子节点数组
+
+// 用叶子节点数组绘图
+const svg = d3.select('#treemap')
+    .append('svg')
+    .attr('width', width)
+    .attr('height', height)
+const g = svg.selectAll('.rects')
+    .data(leaves)
+    .enter()
+    .append('g')
+    .attr('class', 'rects')
+
+// 添加矩阵
+g.append('rect')
+    .attr('x', (d) => d.x0)
+    .attr('y', (d) => d.y0)
+    .attr('width', (d) => (d.x1 - d.x0))
+    .attr('height', (d) => (d.y1 - d.y0))
+    .style('fill', (d) => color(d.parent.data.name))
+    .style('stroke', '#cccccc')
+
+// 添加描述
+g.append('text')
+    .attr('x', (d) => (d.x1 - d.x0) / 2 + d.x0)
+    .attr('y', (d) => (d.y1 - d.y0) / 2 + d.y0)
+    .attr('dx', (d) => { return -d.data.name.length / 2 + 'em' })
+    .attr('dy', (d) => { return '-0.5em' })
+    .text((d) => { return d.data.name })
+    .attr('font-size', (d) => { return 14 - d.depth + 'px' })
+    .attr('fill', '#f0f0f0')
+
+g.append('text')
+    .attr('x', (d) => (d.x1 - d.x0) / 2 + d.x0)
+    .attr('y', (d) => (d.y1 - d.y0) / 2 + d.y0)
+    .attr('dx', (d) => { return -(d.value.toString().length + 2) / 4 + 'em' })
+    .attr('dy', (d) => { return '1em' })
+    .text((d) => { return '(' + d.value + ')' })
+    .attr('font-size', (d) => { return 14 - d.depth + 'px' })
+    .attr('fill', '#ffffff')
